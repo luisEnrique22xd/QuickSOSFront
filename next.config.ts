21cd823e88571f33f03d857f9b70208c828eae6b
unknown <jersainmarinjuarez@gmail.com>
2025-12-06 16:19:28 -0600

    Correciones en frontend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = withPWA({
+  dest: "public",              
+  disable: !isProd,            
+  register: true,              
+  skipWaiting: true,           
+  buildExcludes: [/middleware-manifest\.json$/], 
+})({
+  reactStrictMode: true,
+  experimental: {
+    
+  },
+  images: {
+    domains: [],               
+  },
+}) as NextConfig;
 
 export default nextConfig;
